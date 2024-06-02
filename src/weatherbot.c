518e46bb99d65a1ba462bfd4d85b1f639d6d6a96
AhmedNGaber <ahmed.n.gaber@gmail.com>
2024-06-02 14:38:34 +0200

    Use telebot library with a telegram bot
    Signed-off-by: AhmedNGaber <ahmed.n.gaber@gmail.com>

--- src/weatherbot.c
+++ src/weatherbot.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <apr_file_io.h>
-
+#include <telebot.h>
 #include "weatherbot_rust_wrapper.h"
 
-
+#define SIZE_OF_ARRAY(array) (sizeof(array) / sizeof(array[0]))
 #define TOKEN_MAX_SIZE 128
 
 int main(int argc, char *argv[]) {
@@ -55,7 +56,6 @@ int main(int argc, char *argv[]) {
     printf("Welcome to weathocastbot\n");
     printf("Token: %s\n", token);
 
-    //TBD: Call telebot functions with token
     //TBD: Call Rust functions
     set_city_name("Cairo");
     set_city_geometry(55.12345, 66.12345);
@@ -67,6 +67,84 @@ int main(int argc, char *argv[]) {
     double temperature;
     temperature = get_city_temperature();
     printf("Temperature: %f\n", temperature);
+
+
+    //For Testing: Call telebot functions with token
+    telebot_user_t me;
+    telebot_handler_t handle;
+    if (telebot_create(&handle, token) != TELEBOT_ERROR_NONE)
+    {
+        printf("Telebot create failed\n");
+        return -1;
+    }
+
+
+    if (telebot_get_me(handle, &me) != TELEBOT_ERROR_NONE)
+    {
+        printf("Failed to get bot information\n");
+        telebot_destroy(handle);
+        return -1;
+    }
+
+    printf("ID: %d\n", me.id);
+    printf("First Name: %s\n", me.first_name);
+    printf("User Name: %s\n", me.username);
+
+    telebot_put_me(&me);
+
+    int index, count, offset = -1;
+    telebot_error_e ret;
+    telebot_message_t message;
+    telebot_update_type_e update_types[] = {TELEBOT_UPDATE_TYPE_MESSAGE};
+
+    while (1)
+    {
+        telebot_update_t *updates;
+        ret = telebot_get_updates(handle, offset, 20, 0, update_types, 0, &updates, &count);
+        if (ret != TELEBOT_ERROR_NONE)
+            continue;
+        printf("Number of updates: %d\n", count);
+        for (index = 0; index < count; index++)
+        {
+            message = updates[index].message;
+            if (message.text)
+            {
+                printf("%s: %s \n", message.from->first_name, message.text);
+                if (strstr(message.text, "/dice"))
+                {
+                    telebot_send_dice(handle, message.chat->id, false, 0, "");
+                }
+                else
+                {
+                    char str[4096];
+                    if (strstr(message.text, "/start"))
+                    {
+                        snprintf(str, SIZE_OF_ARRAY(str), "Hello %s, Nice to see you here\n"\
+                                                          "if you want to know current temperature\n"\
+                                                          "of any city, just send the city name\n"\
+                                                          "or the Latitude and longitude in this format \"12.34500:34.56700\"\n"
+                                                          , message.from->first_name);
+                    }
+                    else
+                    {
+                        snprintf(str, SIZE_OF_ARRAY(str), "Current temprature for <b>%s</b> is:", message.text);
+                    }
+                    ret = telebot_send_message(handle, message.chat->id, str, "HTML", false, false, updates[index].message.message_id, "");
+                }
+                if (ret != TELEBOT_ERROR_NONE)
+                {
+                    printf("Failed to send message: %d \n", ret);
+                }
+            }
+            offset = updates[index].update_id + 1;
+        }
+        telebot_put_updates(updates, count);
+
+        sleep(1);
+    }
+
+    telebot_destroy(handle);
+
     goto exit;
 
 apr_error:
